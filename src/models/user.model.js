97152e9a43cd8a28ac01bcc88dd263ac51c302ec
rajatcodingwarrior <rajatrajmaths27up@gmail.com>
2025-10-12 22:22:59 +0530

    added bcrypt and jwt and modified the models and added pre hook middleware

--- src/models/user.model.js
+++ src/models/user.model.js
@@ -1,4 +1,6 @@
 import mongoose, {Schema} from "mongoose";
+import jwt from "jsonwebtoken";
+import bcrypt from "bcrypt";
 
 const userSchema = new Schema({
   username:{
@@ -44,4 +46,45 @@ const userSchema = new Schema({
   timestamps: true
   }
 )
+
+//hash password before saving user
+userSchema.pre("save", async function(next){
+  if(!this.isModified("password")) return next();
+  
+  this.password = bcrypt.hash(this.password,10) 
+  next()
+})
+
+userSchema.methods.isPasswordCorrect = async function 
+(password){
+  return await bcrypt.compare(password, this.password);
+}
+
+userSchema.methods.generateAccessToken = function (){
+  return jwt.sign(
+    {
+      _id: this._id,
+      username: this.username,
+      email: this.email,
+      fullname: this.fullname,
+    },
+    process.env.ACESS_TOKEN_SECRET,
+    {
+      expiresIn: process.env.ACESS_TOKEN_EXPIRY
+    }
+  )}
+
+
+  userSchema.methods.generateRefreshToken = function (){
+  return jwt.sign(
+    {
+      _id: this._id,
+    },
+    process.env.REFRESH_TOKEN_SECRET,
+    {
+      expiresIn: process.env.REFRESH_TOKEN_EXPIRY
+    }
+  )}
+
+
 export const User = mongoose.model("User", userSchema);
