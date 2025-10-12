e296a4285c7662c2fd3d93cfe170e9f9847afddc
rajatcodingwarrior <rajatrajmaths27up@gmail.com>
2025-10-13 01:29:33 +0530

    errors solved and user conroller written project running fine

--- src/app.js
+++ src/app.js
@@ -13,4 +13,13 @@ app.use(express.urlencoded({ extended: true,limit:"16kb"}));
 app.use(express.static('public'));
 app.use(cookieParser());
 
+//routes import
+
+import userRouter from './routes/user.routes.js'; // default export only then this import will work
+
+//routes declaration
+app.use('/api/v1/users',userRouter);
+
+//   https://localhost:4000/api/v1/users
+
 export {app};
