2a187eabb923e549a513c2209017529681559867
rajatcodingwarrior <rajatrajmaths27up@gmail.com>
2025-10-12 10:37:19 +0530

    connectDB in index.js and added middlewares in app.js like cors cookie-parser json urlencoder and added cors_origin in .env

--- src/index.js
+++ src/index.js
@@ -6,4 +6,12 @@ dotenv.config(
   }
 );
 
-connectDB();
+connectDB()
+.then(() => {
+  app.listen(process.env.PORT||8000, () => {
+    console.log(`Server is running on port ${process.env.PORT||8000}`);
+  })
+})
+.catch((err) => {
+  console.log("DB CONNECTION FAILED !!!!", err);
+})
